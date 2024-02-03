4ba1dddefd4a759a42e20d5cef4777e89b5a6b68
Mohamed Al Asam <alasamm408@gmail.com>
2024-02-03 16:24:30 +0200

    init

---
+++ Makefile
@@ -0,0 +1,32 @@
+CFLAGS = \
+	-pedantic \
+	-fno-builtin \
+	-nostdlib \
+	-o2 \
+	-g \
+	-ffreestanding \
+	-Wall \
+	-Wextra \
+	-Werror \
+	-m32 \
+	-fno-exceptions \
+	-fno-leading-underscore \
+	-c 
+
+.PHONY: all clean build run debug
+
+all: clean build run
+
+clean:
+	rm -rf build/*
+
+build:
+	x86_64-elf-as --32 src/kernel/arch/x86/boot.s -o build/boot.o
+	x86_64-elf-as --32 src/kernel/arch/x86/crti.s -o build/crti.o
+	x86_64-elf-as --32 src/kernel/arch/x86/crtn.s -o build/crtn.o
+	x86_64-elf-gcc $(CFLAGS) src/kernel/main.c -o build/kernel.o
+	x86_64-elf-ld -T src/kernel/arch/x86/linker.ld -m elf_i386 -o build/os build/boot.o build/kernel.o build/crti.o build/crtn.o 
+
+run: build/os
+	qemu-system-x86_64 -kernel build/os
+debug:
---
+++ src/kernel/main.c
@@ -0,0 +1,59 @@
+typedef char int8_t;
+typedef unsigned char uint8_t;
+
+typedef short int16_t;
+typedef unsigned short uint16_t;
+
+typedef int int32_t;
+typedef unsigned int uint32_t;
+
+uint16_t *vga = (uint16_t*) 0xb8000;
+void outb(uint16_t port, uint8_t val) {
+    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+uint8_t inb(uint16_t port) {
+    uint8_t ret;
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port) : "memory");
+    return ret;
+}
+
+enum vga_color {
+	VGA_COLOR_BLACK = 0,
+	VGA_COLOR_BLUE = 1,
+	VGA_COLOR_GREEN = 2,
+	VGA_COLOR_CYAN = 3,
+	VGA_COLOR_RED = 4,
+	VGA_COLOR_MAGENTA = 5,
+	VGA_COLOR_BROWN = 6,
+	VGA_COLOR_LIGHT_GREY = 7,
+	VGA_COLOR_DARK_GREY = 8,
+	VGA_COLOR_LIGHT_BLUE = 9,
+	VGA_COLOR_LIGHT_GREEN = 10,
+	VGA_COLOR_LIGHT_CYAN = 11,
+	VGA_COLOR_LIGHT_RED = 12,
+	VGA_COLOR_LIGHT_MAGENTA = 13,
+	VGA_COLOR_LIGHT_BROWN = 14,
+	VGA_COLOR_WHITE = 15,
+};
+
+static inline uint8_t vga_entry_color(enum vga_color fg, enum vga_color bg) {
+	return fg | bg << 4;
+}
+
+static inline uint16_t vga_entry(unsigned char uc, uint8_t color) {
+	return (uint16_t) uc | (uint16_t) color << 8;
+}
+
+void clear_screen() {
+    uint8_t color = vga_entry_color(VGA_COLOR_RED, VGA_COLOR_WHITE);
+    for (uint16_t i = 0; i < (160 * 25); i++) {
+        vga[i] = vga_entry(' ', color);
+    }
+}
+
+void kernel_main(void) {
+    
+    clear_screen();
+    while (1);
+}
